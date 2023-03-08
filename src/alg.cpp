62ae500e5435c53d7d531654e2be72d501a957f2
i44p <jsbbdjzjsjawk+@gmail.com>
2023-03-08 15:43:18 +0300

    implement binary search counter

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int search_leftmost(int* arr, int value, int left, int right) {
+  if (left > right) {
+    return -1;
+  }
+
+  int cursor = (left + right) / 2;
+
+  if (arr[cursor] == value) return cursor;
+  if (arr[cursor] < value) {
+    return search_leftmost(arr, value, cursor + 1, right);
+  }
+  return search_leftmost(arr, value, left, cursor);
+}
+
+int cbinsearch(int* arr, int size, int value) {
+  int left = search_leftmost(arr, value, 0, size - 1);
+  int right = left;
+  while (arr[right] == value) right++;
+  return right - left;
 }
